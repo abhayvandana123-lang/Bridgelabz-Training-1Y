719b82fca9882b97300a6f30395339b278c1781e
Abhay Mishra <you@example.com>
2026-01-26 22:27:32 +0530

    ass 3

---
+++ basic-program/DigitCounter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DigitCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+
+        int n = Math.abs(num);
+        int count = 0;
+
+        if (n == 0) {
+            count = 1;
+        } else {
+            while (n > 0) {
+                n /= 10;
+                count++;
+            }
+        }
+
+        System.out.println("Number of digits: " + count);
+        sc.close();
+    }
+}
