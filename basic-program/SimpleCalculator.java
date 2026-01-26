79074c2c2e9f098eceb3e6c8cff6c3d7691a4b59
Abhay Mishra <you@example.com>
2026-01-26 22:39:46 +0530

    ass 3

---
+++ basic-program/SimpleCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                if (b != 0)
+                    System.out.println(a / b);
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+
+        sc.close();
+    }
+}
