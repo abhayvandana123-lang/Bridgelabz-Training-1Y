b81be2e374202ea37ea7384ec5c7591713d9e7b5
Abhay Mishra <you@example.com>
2026-01-26 21:47:43 +0530

    ass 3

---
+++ basic-program/FriendsComparison.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FriendsComparison {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter Amar's age: ");
+        int ageAmar = s.nextInt();
+        System.out.print("Enter Akbar's age: ");
+        int ageAkbar = s.nextInt();
+        System.out.print("Enter Anthony's age: ");
+        int ageAnthony = s.nextInt();
+
+        System.out.print("Enter Amar's height: ");
+        double heightAmar = s.nextDouble();
+        System.out.print("Enter Akbar's height: ");
+        double heightAkbar = s.nextDouble();
+        System.out.p
