043c899d3db06daac3312ceeae863dea50b856ed
abhayvandana123-lang <abhayvandana123@gmail.com>
2026-04-29 11:36:57 +0530

    added logistic problem

---
+++ logistic/Driver.java
@@ -0,0 +1,31 @@
+public class Driver {
+    private String driverId;
+    private String name;
+    private RouteLinkedList<Checkpoint> routeHistory;
+
+    public Driver(String driverId, String name) {
+        this.driverId = driverId;
+        this.name = name;
+        this.routeHistory = new RouteLinkedList<>();
+    }
+
+    public void addCheckpoint(Checkpoint checkpoint) {
+        routeHistory.addCheckpoint(checkpoint);
+    }
+
+    public void printDriverReport() {
+        System.out.println("Driver: " + driverId + " - " + name);
+        System.out.println("Route Summary:");
+        routeHistory.printRoute();
+
+        double totalDistance = routeHistory.computeTotalDistance();
+        double totalPenalty = routeHistory.computeTotalPenalty();
+        double routeScore = totalDistance - totalPenalty;
+
+        System.out.println("Total Distance: " + totalDistance + " km");
+        System.out.println("Total Penalty: " + totalPenalty);
+        System.out.println("Route Score: " + routeScore);
+        System.out.println("Critical Route Check: " +
+                (routeHistory.checkConsistency() ? "All required checkpoints present" : "Missing critical checkpoints"));
+    }
+}
